72efe44d9227a6df4ffe8ae1a050c01951801958
anujg1991 <guptaanuj007@gmail.com>
2017-11-23 13:18:55 -0500

    Added bubbles

--- map/d3_tom/dataEdit.R
+++ map/d3_tom/dataEdit.R
@@ -1,10 +1,22 @@
 library(readr)
 
-AIDS_Deaths <- read_csv("~/GitHub/InfoViz-Project/map/d3_tom/data/AIDS_Deaths.csv")
+GDP <- read_csv("~/GitHub/InfoViz-Project/map/d3_tom/data/GDP.csv")
 id <- read_csv("~/GitHub/InfoViz-Project/map/d3_tom/data/id3Country.csv")
+
+colnames(GDP)[2:length(GDP)]
+
+d <- data.frame()
+for (i in 2:length(GDP)){
+  a <- GDP[,c(1,i)]
+  b <- rep(colnames(GDP)[i],nrow(a))
+  c <- cbind(a,b)
+  colnames(c) <- c('id','value','year')
+  d <- rbind(d,c)
+}
+
 
 colnames(AIDS_Deaths)[1]  <- "country"
 
 a <- merge(id,AIDS_Deaths,by = "country")
 
-write.csv(a,'~/GitHub/InfoViz-Project/map/d3_tom/data/AIDS_Deaths1.csv')
+write.csv(d,'~/GitHub/InfoViz-Project/map/d3_tom/data/GDP1.csv')
--- map/d3_tom/proc_emdat.py
+++ map/d3_tom/proc_emdat.py
@@ -1,30 +1,30 @@
 import pandas as pd
 import json
 
-fname = "data/AIDS_DEATHS.csv"
-fname_out = "data/AIDS_DEATHS.json"
+fname = "data/GDP1.csv"
+fname_out = "data/GDP.json"
 
-cols = ['country','id','Subgroup','Year','Source','Unit','Value','Value Footnotes']
+cols = ['id','value','year']
 
 df = pd.read_csv(fname, sep=',', names=cols, header=0)
 print (df.shape)
 
-df = df.dropna(axis=0, subset=['Value'])
+df = df.dropna(axis=0, subset=['value'])
 print ("shape after nan removal: {0}".format(df.shape))
 
 #df = df.groupby('disastertype')
 
 dict_json = {}
 #dict_json["Storm"] = {}
-dict_json["Deaths"] = {}
+dict_json["GDP"] = {}
 #dfg = df.get_group("Storm").groupby('year')
-dfg = df.groupby('Year')
+dfg = df.groupby('year')
 
 for k,v in dfg:
     if k in range(1990,2015):
         print (k)
         v.index = v['id']
-        dict_json['Deaths'][k] = v['Value'].to_dict()
+        dict_json['GDP'][k] = v['value'].to_dict()
         
 with open(fname_out, 'w') as f:
     f.write(json.dumps(dict_json))
